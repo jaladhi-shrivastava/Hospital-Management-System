a52acf0896459a534f4224ea00f2dcdddd6caa79
ayushishri04 <ayushiishrii20@gmail.com>
2026-03-30 23:35:57 +0530

    Resolved merge conflict in PatientViewController

--- src/main/java/com/hms/controller/view/PatientViewController.java
+++ src/main/java/com/hms/controller/view/PatientViewController.java
@@ -1,22 +1,11 @@
 package com.hms.controller.view;
 
-<<<<<<< HEAD:src/main/java/com/hms/module/patient/PatientViewController.java
-import com.hms.entity.Patient;
-import com.hms.entity.Stay;
-=======
 import com.hms.service.module.patient.PatientModuleService;
->>>>>>> origin/main:src/main/java/com/hms/controller/view/PatientViewController.java
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/patients")
@@ -35,5 +24,4 @@ public class PatientViewController {
 
         return "patient/dashboard";
     }
-
 }
